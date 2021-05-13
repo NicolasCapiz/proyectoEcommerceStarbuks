ea5f98fe2cf7417f593346fa7c4bd2223b26c404
Tomy97 <64372041+Tomy97@users.noreply.github.com>
2021-05-13 19:32:29 -0300

    agregado del json con los productos y desestructuracion

--- proyecto-ecommerce-starbuks/src/app/Pages/Tienda/tienda/tienda.component.ts
+++ proyecto-ecommerce-starbuks/src/app/Pages/Tienda/tienda/tienda.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core'
+// import { Bebidas } from 'src/app/Bebidas'
+import * as cafes from '../../../../assets/data/bebidasCafes.json'
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.component.html',
-  styleUrls: ['./tienda.component.css']
+  styleUrls: ['./tienda.component.css'],
 })
 export class TiendaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(cafes.bebidasHeladas.producto1)
   }
 
+  ngOnInit(): void {}
 }
--- proyecto-ecommerce-starbuks/src/app/app.module.ts
+++ proyecto-ecommerce-starbuks/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { SectionComponent } from './shared/section/section.component'
@@ -20,7 +21,7 @@ import { FooterComponent } from './shared/footer/footer.component'
     HomeComponent,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
